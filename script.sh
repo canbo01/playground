cbdac76e2b8a34e442a2c377697b1d3c0f3bac87
Can Boran Püsküllüoğlu <78820095+canbo01@users.noreply.github.com>
2021-07-22 02:30:51 +0300

    first commit
    first commit for script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
+kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
+
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+
+yum install -y initscripts net-tools.x86_64 vim openssh-server initscripts
+netstat -tulpn
