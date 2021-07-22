917901a8daf235a9318f7fbfa2f31a7351c2da0e
Can Boran Püsküllüoğlu <78820095+canbo01@users.noreply.github.com>
2021-07-22 15:09:50 +0300

    order was changed
    order was changed

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
+yum update -y
+yum install -y initscripts net-tools.x86_64 vim openssh-server initscripts
+
 kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
 kubectl apply -f https://raw.githubusercontent.com/cloudnativelabs/kube-router/master/daemonset/kubeadm-kuberouter.yaml
 
@@ -5,6 +8,4 @@ mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-yum update -y
-yum install -y initscripts net-tools.x86_64 vim openssh-server initscripts
 netstat -tulpn
